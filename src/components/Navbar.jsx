7ad0b962ce75dcfc92414114046be25bde91b777
njange <jamesnjange80@gmail.com>
2024-06-03 23:01:08 +0300

    navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,18 @@ const Navbar = () => {
     }
   return (
     <div className='max-w-[1200px] bg-red-200 h-24 mx-auto flex justify-between items-center text-lg'>
-        Navbar
+
+        <h1 className='text-3xl font-bold'>J. DOE</h1>
+        
+        <ul className='hidden md:flex'>
+            <li className='p-5'><link to='/about'>About</link></li>
+            <li className='p-5'><link to='/about'>Experience</link></li>
+            <li className='p-5'><link to='/about' className='px-4 py-2 rounded-xl bg-green-300'>Contact</link></li>
+        </ul>
+
+        <div>
+
+        </div>
     </div>
   )
 }
