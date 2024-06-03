691644ed93a7a21152c827fca8bf0b7f70d415d2
njange <jamesnjange80@gmail.com>
2024-06-03 22:43:24 +0300

    NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
   return (
-    <div>Navbar</div>
+    <div className='max-w-[1200px] bg-red-200 h-24 mx-auto flex justify-between items-center text-lg'>
+        Navbar
+    </div>
   )
 }
 
