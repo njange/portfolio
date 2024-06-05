6432fc89d4ae0d9a7a99b400642f515a717cfbed
njange <jamesnjange80@gmail.com>
2024-06-05 14:07:32 +0300

    solved blank screen problem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,34 @@
-import React,{ useState } from 'react'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import React, { useState } from "react"
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false)
-
-    const handleNav = () => {
-        setNav(!nav)
-    }
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
-    <div className='max-w-[1200px] bg-red-200 h-24 mx-auto flex justify-between items-center text-lg'>
+    <div className="max-w-[1200px] bg-red-200 h-24 mx-auto flex justify-between items-center text-lg">
+      <h1 className="text-3xl font-bold">J. DOE</h1>
 
-        <h1 className='text-3xl font-bold'>J. DOE</h1>
-        
-        <ul className='hidden md:flex'>
-            <li className='p-5'><link to='/about'>About</link></li>
-            <li className='p-5'><link to='/about'>Experience</link></li>
-            <li className='p-5'><link to='/about' className='px-4 py-2 rounded-xl bg-green-300'>Contact</link></li>
-        </ul>
+      <ul className="hidden md:flex">
+        <li className="p-5">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="p-5">
+          <Link to="/about">Experience</Link>
+        </li>
+        <li className="p-5">
+          <Link to="/about" className="px-4 py-2 rounded-xl bg-green-300">
+            Contact
+          </Link>
+        </li>
+      </ul>
 
-        <div>
-
-        </div>
+      <div>{nav ? "" : ""}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
