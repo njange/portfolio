62c198995f83776a270a7be3e10521e78597e611
njange <jamesnjange80@gmail.com>
2024-06-05 14:29:55 +0300

    side navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,25 @@ const Navbar = () => {
         </li>
       </ul>
 
-      <div>{nav ? "" : ""}</div>
+        <div onClick={handleNav} className='block md:hidden z-40'>
+            {nav ? <AiOutlineMenu size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+        <div className={nav ? 'z-40 text-gray-300 fixed left-0 top-0 h-full w-[60%] border-r border-r-gray-900 bg-[#171717] ease-in-out duration-500'
+            : 'fixed left-[-100%]'}>
+        <ul className="p-8 text-2xl">
+        <li className="p-5">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="p-5">
+          <Link to="/about">Experience</Link>
+        </li>
+        <li className="p-5">
+          <Link to="/about" className="px-4 py-2 rounded-xl bg-green-300">
+            Contact
+          </Link>
+        </li>
+      </ul>
+        </div>
     </div>
   );
 };
