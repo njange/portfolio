278c96b5a5a900718c9988d158321599bb5abd02
njange <jamesnjange80@gmail.com>
2024-06-05 17:39:43 +0300

    Hero dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import profilepic from '../images/profilepic.jpg'
+import { AiFillLinkedin, AiFillGithub, AiFillTwitterSquare } from 'react-icons/ai'
+import { TypeAnimation } from 'react-type-animation'
+import {
+    DiJavascript1,
+    DiReact,
+    DiNodejsSmall,
+    DiHtml,
+    DiCss,
+    DiSass,
+    DiBootstrap
+} from 'react-icons/di'
 
 const Hero = () => {
   return (
     <div>
-        
+
     </div>
   )
 }
