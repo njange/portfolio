cf2fb155cbaa825f8ae5c2923de2467eabc9b97a
njange <jamesnjange80@gmail.com>
2024-06-06 17:32:02 +0300

    Tech stack

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   return (
     <div className='max-w-[1200px] mx-auto grid md:grid-cols-8 gap-6 p-10 md:p-0' id="home">
 
-        <div className="grid grid-cols-2 md:cols-span-5 p-8 glass">
+        <div className="md:cols-span-5 p-8 glass grid grid-cols-2">
 
             <img className="w-[800px]" src={profilepic} alt='profilepic' />
 
@@ -33,13 +33,29 @@ const Hero = () => {
                         ]}
                         wrapper="span"
                         speed={50}
-                        repeat={infinity}
+                        repeat={Infinity}
                     />
 
                 </p>
                 <p className='text-xl md:text-3xl font-semibold text-gray-400'>With a 5+ years experience</p>
                 <button className='mt-4 px-4 py-2 text-lg font-bold text-white bg-primary-color rounded-xl'>Download CV</button>
             </div>
+        </div>
+        <div className='grid md:col-span-3 gap-6'>
+            <div className='text-5xl p-12 glass'>
+                <p className='text-gray-200 text-xl font-bold mb-4 text-center'>My Tech Stack</p>
+                <div className='grid grid-cols-4 gap-4'>
+                    <DiHtml5 size={50} className='text-red-500' />
+                    <DiCss3 size={50} className='text-blue-500' />
+                    <DiJavascript1 size={50} className='text-yellow-500' />
+                    <DiReact size={50} className='text-blue-300' />
+                    <DiNodejsSmall size={50} className='text-green-500' />
+                    <DiSass size={50} className='text-pink-500' />
+                    <DiBootstrap size={50} className='text-purple-500' />
+                    
+                </div>
+            </div>
+
         </div>
 
     </div>
