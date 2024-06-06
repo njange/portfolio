2c45972b0841df03128edc1af97321549667c1df
njange <jamesnjange80@gmail.com>
2024-06-06 17:03:37 +0300

    download button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,9 +16,9 @@ const Hero = () => {
   return (
     <div className='max-w-[1200px] mx-auto grid md:grid-cols-8 gap-6 p-10 md:p-0' id="home">
 
-        <div className="grid grid-cols-2 md:cols-span-5 p-8">
+        <div className="grid grid-cols-2 md:cols-span-5 p-8 glass">
 
-            <img className="" src={profilepic} alt='profilepic' />
+            <img className="w-[800px]" src={profilepic} alt='profilepic' />
 
             <div className="my-auto ml-8 flex-col text-gray-200">
                 <p className="text-2xl md:text-4xl font-bold"> Hello, I'm James<br/>
@@ -32,9 +32,13 @@ const Hero = () => {
                             1000,
                         ]}
                         wrapper="span"
+                        speed={50}
+                        repeat={infinity}
                     />
-                    
+
                 </p>
+                <p className='text-xl md:text-3xl font-semibold text-gray-400'>With a 5+ years experience</p>
+                <button className='mt-4 px-4 py-2 text-lg font-bold text-white bg-primary-color rounded-xl'>Download CV</button>
             </div>
         </div>
 
