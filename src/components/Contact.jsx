7ab4f79786c0da87292a5c0e169a838d04ebd296
njange <jamesnjange80@gmail.com>
2024-06-07 18:25:48 +0300

    contact description

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='px-6 max-w-[1200px] mx-auto grid md:grid-cols-2 place-items-center' id='contact'>
+        <div>
+            <div className='text-gray-300 my-3 p-2'>
+                <h3 className='text-4xl font-semibold mb-5'>Let's <span>Connect</span></h3>
+                <p className='text-justify leading-7'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                     Voluptas maxime numquam velit explicabo et fuga inventore 
+                     modi doloribus, quo autem eos animi alias in asperiores nam voluptates.
+                      Accusantium, assumenda veniam?
+                </p>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
