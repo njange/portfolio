0d4c7033f3a696a6c9f22d1ccdb10bf73052e1c4
njange <jamesnjange80@gmail.com>
2024-06-08 08:23:15 +0300

    About part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import project2 from '../assets/project2.png'
+import project3 from '../assets/project3.png'
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejsSmall,
+  DiMongodb,
+  DiCss3,
+  DiHtml5,
+  DiSass,
+  DiBootstrap,
+  DiGit,
+  DiNpm,
+} from 'react-icons/di'
 
 const About = () => {
   return (
