491428173e1cd0030d136fc8cbc5e3dcfc5de2d6
njange <jamesnjange80@gmail.com>
2024-06-08 09:39:33 +0300

    footer start

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='max-w-[1200px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12'>
+      <div className='space-y-4'>
+        <h3></h3>
+
+      </div>
+    </div>
   )
 }
 
